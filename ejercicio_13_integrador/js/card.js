8916ed47667ad49662a8af7a2b7c8abb76b03db7
paceguma <paceguma@gmail.com>
2022-12-10 14:14:11 -0300

    Agregando archivos js y formulario

---
+++ ejercicio_13_integrador/js/card.js
@@ -0,0 +1,36 @@
+function Card(heading, description, image) {
+    this.heading = heading;
+    this.description = description;
+    this.image = image;
+
+    this.appendTo = function () {
+        let card = document.createElement('a')
+        card.classList.add('card')
+        card.href = 'https://sony.com'
+
+        card.innerHTML = `
+        
+        <article class="card__article">
+        <div class="card__image-container">
+            <img class="card__image" src=${image} alt=${heading}/>
+        </div>
+        <div class="card__content">
+            <h2 class="card__heading">${heading}</h2>
+            <div class="card__description">
+            <p>${description}</p>
+            </div>
+        </div>
+        </article>
+        `
+    }
+
+}
+
+const elemCardContainer = document.getElementsByClassName()
+
+
+const card1 = new Card('Tv', 'Lorem ijja amtntn', 'img/productos/Artesania (17).png')
+const card2 = new Card('Celuar', 'Lorem ijja amtntn', 'img/productos/Artesania (18).png')
+
+console.log(card1);
+console.log(card2);
