fe4d617027616928cfbc07d3f17f3916a5af0aa1
paceguma <paceguma@gmail.com>
2022-12-11 20:59:13 -0300

    Ajustes varios, creacion de nuevas plantillas

--- ejercicio_13_integrador/js/card.js
+++ ejercicio_13_integrador/js/card.js
@@ -1,29 +1,29 @@
 function initInicio() {
-    // console.warn('initInicio');
+  // console.warn('initInicio');
 
-    function Card(heading, description, image) {
-        this.heading = heading;
-        this.description = description;
-        this.image = image;
+  function Card(heading, description, image) {
+    this.heading = heading
+    this.description = description
+    this.image = image
 
-        this.appendTo = function (destinationElement) {
-            let card = document.createElement('a')
-            card.classList.add('card')
-            card.href = 'https://sony.com'
+    this.appendTo = function (destinationElement) {
+      let card = document.createElement("a")
+      card.classList.add("card")
+      card.href = "https://sony.com"
 
-            let that = this
-            card.addEventListener('click', (e) => {
-                e.preventDefault()
+      let that = this
+      card.addEventListener("click", (e) => {
+        e.preventDefault()
 
-                let nuevoItemCarrito = document.createElement('div')
-                nuevoItemCarrito.classList.add('carrito__item')
-                nuevoItemCarrito.innerHTML = that.heading
-                console.log(nuevoItemCarrito);
+        let nuevoItemCarrito = document.createElement("div")
+        nuevoItemCarrito.classList.add("carrito__item")
+        nuevoItemCarrito.innerHTML = that.heading
+        console.log(nuevoItemCarrito)
 
-                elemSectionCarrito.append(nuevoItemCarrito)
-            })
+        elemSectionCarrito.append(nuevoItemCarrito)
+      })
 
-            card.innerHTML = `        
+      card.innerHTML = `        
         <article class="card__article">
         <div class="card__image-container">
             <img class="card__image" src=${image} alt=${heading}/>
@@ -36,45 +36,57 @@ function initInicio() {
         </div>
         </article>
         `
-            destinationElement.appendChild(card)
-        }
-
+      destinationElement.appendChild(card)
     }
+  }
 
-    const elemCardContainer = document.getElementsByClassName('cards-container')[0]
+  const elemCardContainer =
+    document.getElementsByClassName("cards-container")[0]
 
-    const card1 = new Card('Tv', 'Lorem ijja amtntn', 'img/productos/conjunto.JPG')
-    const card2 = new Card('Celular', 'Lorem ijja amtntn', 'img/productos/Artesania (18).png')
-    const card3 = new Card('Heladera', 'Lorem ijja amtntn', 'img/productos/Artesania (16).png')
-    const card4 = new Card('Tablet', 'Lorem ijja amtntn', 'img/productos/Artesania (14).png')
-    const card5 = new Card('Camara', 'Lorem ijja amtntn', 'img/productos/Artesania (10).png')
+  const card1 = new Card(
+    "Tv",
+    "Lorem ijja amtntn",
+    "img/productos/conjunto.JPG"
+  )
+  const card2 = new Card(
+    "Celular",
+    "Lorem ijja amtntn",
+    "img/productos/Artesania (18).png"
+  )
+  const card3 = new Card(
+    "Heladera",
+    "Lorem ijja amtntn",
+    "img/productos/Artesania (16).png"
+  )
+  const card4 = new Card(
+    "Tablet",
+    "Lorem ijja amtntn",
+    "img/productos/Artesania (14).png"
+  )
+  const card5 = new Card(
+    "Camara",
+    "Lorem ijja amtntn",
+    "img/productos/Artesania (10).png"
+  )
 
-    const cards = [
-        card1,
-        card2,
-        card3,
-        card4,
-        card5
-    ]
+  const cards = [card1, card2, card3, card4, card5]
 
-    // console.log(cards);
+  // console.log(cards);
 
+  // card1.appendTo(elemCardContainer)
+  // card2.appendTo(elemCardContainer)
+  // card3.appendTo(elemCardContainer)
+  // card4.appendTo(elemCardContainer)
+  // card5.appendTo(elemCardContainer)
 
-    // card1.appendTo(elemCardContainer) 
-    // card2.appendTo(elemCardContainer) 
-    // card3.appendTo(elemCardContainer) 
-    // card4.appendTo(elemCardContainer) 
-    // card5.appendTo(elemCardContainer) 
+  // console.log(cards[0].heading);
+  // console.log(cards[0].image);
 
-    // console.log(cards[0].heading);
-    // console.log(cards[0].image);
+  // cards[0].appendTo(elemCardContainer)
+  // cards[1].appendTo(elemCardContainer)
+  // cards[2].appendTo(elemCardContainer)
 
-    // cards[0].appendTo(elemCardContainer)
-    // cards[1].appendTo(elemCardContainer)
-    // cards[2].appendTo(elemCardContainer)
-
-    for (const unaCard of cards) {
-        unaCard.appendTo(elemCardContainer)
-    }
+  for (const unaCard of cards) {
+    unaCard.appendTo(elemCardContainer)
+  }
 }
-
