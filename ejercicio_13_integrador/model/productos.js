5ea7fef3bda05efe11d3433df0471e0df1b031b2
paceguma <paceguma@gmail.com>
2022-12-17 10:08:58 -0300

    Agregando url mongo

--- ejercicio_13_integrador/model/productos.js
+++ ejercicio_13_integrador/model/productos.js
@@ -16,3 +16,37 @@ const productoSchema = mongoose.Schema({
 
 //Modelo del doc almacenado en una coleccion 
 const ProductoModel = mongoose.model('productos', productoSchema)
+
+//Conexion a la base de datos
+class ProductoModelMongoDB {
+   async conectarDB(){
+    try {
+      await mongoose.connect(process.env.URI_MONGODB_LOCAL)
+      console.log(`Base de datos conectada`)
+    } catch (error) {
+      console.error(`MongoDB error al conectar ${error}`);
+    }
+  }
+
+  async crearProducto(producto){
+
+  }
+
+  async leerProductos(){
+
+  }
+
+  async leerProducto(){
+
+  }
+
+  async actualizarProducto(id, producto){
+
+  }
+
+  async borrarProducto(id){
+
+  }
+}
+
+module.exports = ProductoModelMongoDB
